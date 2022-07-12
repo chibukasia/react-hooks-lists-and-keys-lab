0537b589cfcae2461a49b38ac78ee6b564ca9f89
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-07-12 22:12:54 +0300

    commit tree ad3605be1ee144cd2fdd3be7c4cbd210d754915e

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksElements = links.map(link=>{
+    return <nav>
+        <a key={link} href={link}>{link}</a>
+      </nav>;
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <div>
+    {linksElements}
+  </div>
 }
 
 export default NavBar;
